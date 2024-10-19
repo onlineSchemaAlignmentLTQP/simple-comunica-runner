81ca3ec93ab64ebd3eec9791f8b9e2f8882629de
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-10-19 11:50:55 +0200

    node module ignored

--- node_modules/@comunica/logger-bunyan/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './LoggerBunyan';
-export * from './stream/BunyanStreamProvider';
-export * from './stream/BunyanStreamProviderFile';
-export * from './stream/BunyanStreamProviderStderr';
-export * from './stream/BunyanStreamProviderStdout';
